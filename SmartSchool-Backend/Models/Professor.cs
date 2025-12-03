125c36f33ef02e7b61d2e885a55dcd934c9bd9ca
Eduardolimzz <eduardoaluno1800@gmail.com>
2025-12-03 18:55:34 -0300

    feat: adicionando dotnet ef para exito em conexão com o banco de dados

--- SmartSchool-Backend/Models/Professor.cs
+++ SmartSchool-Backend/Models/Professor.cs
@@ -1,18 +1,19 @@
+using System.Collections.Generic;
+
 namespace SmartSchool_Backend.Models
 {
     public class Professor
     {
         public Professor() { }
 
-        public Professor(int id, string nome, string disciplina)
+        public Professor(int id, string nome)
         {
             this.id = id;
             this.nome = nome;
-            this.disciplina = disciplina;
         }
 
         public int id { get; set; }
         public string nome { get; set; }
-        public string disciplina { get; set; }
+        public IEnumerable<Disciplina> Disciplinas { get; set; }
     }
 }
--- SmartSchool-Backend/Startup.cs
+++ SmartSchool-Backend/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -25,6 +26,10 @@ namespace SmartSchool_Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Data.DataContext>(
+                x => x.UseSqlite(Configuration.GetConnectionString("DefaultConn"))
+            );
+
             services.AddControllers();
         }
 
